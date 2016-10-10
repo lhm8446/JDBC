6a9f25a414a3cb3485c118732da5e2aac5c48ad4
kickscar <kickscar@gmail.com>
2016-10-10 17:39:49 +0900

    jdbc

---
+++ src/com/bit2016/jdbc/HRSearchEmpoyee.java
@@ -0,0 +1,10 @@
+package com.bit2016.jdbc;
+
+public class HRSearchEmpoyee {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ src/com/bit2016/jdbc/vo/BookVo.java
@@ -0,0 +1,41 @@
+package com.bit2016.jdbc.vo;
+
+public class BookVo {
+	private Long no;
+	private String title;
+	private String pubDate;
+	private String state;
+	private Long authorNo;
+	public Long getNo() {
+		return no;
+	}
+	public void setNo(Long no) {
+		this.no = no;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getPubDate() {
+		return pubDate;
+	}
+	public void setPubDate(String pubDate) {
+		this.pubDate = pubDate;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public Long getAuthorNo() {
+		return authorNo;
+	}
+	public void setAuthorNo(Long authorNo) {
+		this.authorNo = authorNo;
+	}
+	
+	
+}
